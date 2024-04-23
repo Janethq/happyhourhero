df87632e45ff5560c53391a9f6f0f1c6eef7f596
Jane <tanhuiqing95@hotmail.com>
2024-04-23 11:55:46 +0800

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,32 @@
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Daily from "./pages/Daily";
 import History from "./pages/History";
 import Favourites from "./pages/Favourites";
 import Navbar from "./components/Navbar";
-import { getRandomCocktail } from "./api/cocktailDB";
-
+// import { getRandomCocktail } from "./api/cocktailDB";
 import "./App.css";
 
-const url = "https://www.thecocktaildb.com/api/json/v1/1/random.php";
+// const url = "https://www.thecocktaildb.com/api/json/v1/1/random.php";
 
-const response = await fetch(url, { method: "GET" });
-const test = await response.json();
-console.log(test);
+// const response = await fetch(url, { method: "GET" });
+// const test = await response.json();
+// console.log(test);
 
 function App() {
-  const [cocktailData, setCocktailData] = useState(null);
+  // const [cocktailData, setCocktailData] = useState(null);
 
-  useEffect(() => {
-    const fetchRandomCocktail = async () => {
-      const cocktail = await getRandomCocktail();
-      // console.log(cocktail);
-      setCocktailData(cocktail);
-    };
-    if (cocktailData === null) {
-      fetchRandomCocktail();
-    }
-  }, ); //empty []= on load
+  // useEffect(() => {
+  //   const fetchRandomCocktail = async () => {
+  //     const cocktail = await getRandomCocktail();
+  //     // console.log(cocktail);
+  //     setCocktailData(cocktail);
+  //   };
+  //   if (cocktailData === null) {
+  //     fetchRandomCocktail();
+  //   }
+  // }, []); //empty []= on load
 
   return (
     <main>
--- src/pages/Daily.jsx
+++ src/pages/Daily.jsx
@@ -1,5 +1,11 @@
 
 
+
+const url = "https://www.thecocktaildb.com/api/json/v1/1/random.php";
+
+const response = await fetch(url, { method: "GET" });
+const test = await response.json();
+console.log(test);
 function Daily() {
   return (
     <div>Daily</div>
