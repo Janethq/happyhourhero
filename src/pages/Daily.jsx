f87b6591d473d34a1e83b309550d597f51c8301c
Jane <tanhuiqing95@hotmail.com>
2024-04-23 12:14:00 +0800

    test

--- src/pages/Daily.jsx
+++ src/pages/Daily.jsx
@@ -1,12 +1,24 @@
+import { useEffect, useState } from "react";
+import { getRandomCocktail } from "../api/cocktailDB";
 
 
+// const url = "https://www.thecocktaildb.com/api/json/v1/1/random.php";
 
-const url = "https://www.thecocktaildb.com/api/json/v1/1/random.php";
+// const response = await fetch(url, { method: "GET" });
+// const test = await response.json();
+// console.log(test);
 
-const response = await fetch(url, { method: "GET" });
-const test = await response.json();
-console.log(test);
 function Daily() {
+  const [cocktail, setCocktail] = useState(null);
+  useEffect(() => {
+    console.log(cocktail) // To check for cocktail on null and the cocktail object
+    if (cocktail === null) { // only gets random cocktail if there isn't one already
+      getRandomCocktail()
+        .then((data) => {
+          setCocktail(data);
+        });
+    }
+  }, [cocktail])
   return (
     <div>Daily</div>
   )
