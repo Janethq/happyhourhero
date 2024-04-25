428574ea8ed12f1b6d3efb17683cf0f8f4669205
Jane <tanhuiqing95@hotmail.com>
2024-04-26 00:03:53 +0800

    import api authentication

--- src/pages/Cocktail.jsx
+++ src/pages/Cocktail.jsx
@@ -1,32 +1,31 @@
-
+import.meta.env.VITE_AIRTABLE_API_KEY;
 
 function Cocktail({ cocktail }) {
-
-const postAirtable = async () => {
-  const url = `https://api.airtable.com/v0/appmAwZOPe64Evw3t/Table%201`;
-  const dataObj = {
-    fields: {
-      name: cocktail.strDrink,
-      cocktailId: cocktail.idDrink,
-    },
+  const postAirtable = async () => {
+    const url = `https://api.airtable.com/v0/appmAwZOPe64Evw3t/Table%201`;
+    const dataObj = {
+      fields: {
+        name: cocktail.strDrink,
+        cocktailId: cocktail.idDrink,
+      },
+    };
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${import.meta.env.VITE_AIRTABLE_API_KEY}`,
+      },
+      body: JSON.stringify(dataObj),
+    };
+    const response = await fetch(url, options);
+    const postResponse = await response.json();
+    console.log(postResponse);
   };
-  const options = {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${import.meta.env.VITE_AIRTABLE_API_KEY}`,
-    },
-    body: JSON.stringify(dataObj),
-  };
-  const response = await fetch(url, options);
-  const postResponse = await response.json();
-  console.log(postResponse);
-};
 
   return (
     <div>
       <h2>Name: {cocktail.strDrink}</h2>
-      <img src={cocktail.strDrinkThumb} width='300' />
+      <img src={cocktail.strDrinkThumb} width="300" />
       <p>{cocktail.strInstructions}</p>
       <br />
 
